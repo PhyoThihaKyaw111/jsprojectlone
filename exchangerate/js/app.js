6d4b0bcc20a9bfc2b4a425a7191162cd2a862926
phyothihakyaw <kyawphyothiha7@gmail.com>
2022-08-06 08:00:41 +0630

    jsproject

---
+++ exchangerate/js/app.js
@@ -0,0 +1,72 @@
+// Ui
+const currencyone = document.getElementById('currency-one'),
+    amountone = document.getElementById('amount-one');
+
+const currencytwo = document.getElementById('currency-two'),
+    amounttwo = document.getElementById('amount-two');
+
+
+const rateel = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+
+
+// Calculate Exchange Rate and Update by AJAX
+function calculate(){
+
+    // console.log('i am working');
+
+    const currone = currencyone.value;
+    const currtwo = currencytwo.value;
+
+
+    fetch(`https://v6.exchangerate-api.com/v6/794a0440671bc6887e1c86f4/latest/${currone}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+
+        const rate = data.conversion_rates[currtwo];
+        // console.log(rate);
+
+        rateel.innerText = `1 ${currone} = ${rate} ${currtwo}`;
+
+        amounttwo.value = (amountone.value * rate).toFixed(2);
+
+
+    });
+
+}
+
+
+
+
+
+
+// Event Listener
+currencyone.addEventListener('change',calculate);
+amountone.addEventListener('input',calculate);
+currencytwo.addEventListener('change',calculate);
+amounttwo.addEventListener('input',calculate);
+
+
+
+
+swap.addEventListener('click',()=>{
+
+    const temp = currencyone.value;
+
+    currencyone.value = currencytwo.value;
+    currencytwo.value = temp;
+
+    calculate();
+    
+
+});
+
+
+calculate();
+
+
+
+// 22 VPN2021 WDFXXX
